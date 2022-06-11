388a9bf4bc12b567439fbc1a784f198e266128b6
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-06-12 00:08:40 +0700

    delete consultation histories model, migration, and routes

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const problemImagesRouter = require("./routes/problem_images");
 const notificationCategoriesRouter = require("./routes/notification_categories");
 const notificationsRouter = require("./routes/notifications");
 const expertsRouter = require("./routes/experts");
-const consultationHistoriesRouter = require("./routes/consultation_histories");
 const usersRouter = require("./routes/users");
 const refreshTokensRouter = require("./routes/refresh_tokens");
 
@@ -32,7 +31,6 @@ app.use("/problem_images", problemImagesRouter);
 app.use("/notification_categories", notificationCategoriesRouter);
 app.use("/notifications", notificationsRouter);
 app.use("/experts", expertsRouter);
-app.use("/consultation_histories", consultationHistoriesRouter);
 app.use("/users", usersRouter);
 app.use("/refresh_tokens", refreshTokensRouter);
 
